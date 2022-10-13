add042c85818eb4fdc90ea2b0981f38d4dfa78b3
Gratien1 <gratiensossouzin1@gmail.com>
2022-10-13 13:00:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+vim 5-printf.c
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of
+
+			art,\n");
+	return (0);
+}
