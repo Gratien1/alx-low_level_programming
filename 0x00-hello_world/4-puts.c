105029845195ba78e90ae378c3e7f307d24cdc0b
Gratien1 <gratiensossouzin1@gmail.com>
2022-10-13 19:22:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Printing
- * Return: Always 0 (Successful)
- *
- */int main(void)
+ * main - Entry
+ *  Return: Always 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
