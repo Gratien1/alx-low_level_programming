2d4e408689af376308fc3a53bf75afd5025b0176
Gratien1 <gratiensossouzin1@gmail.com>
2022-10-19 01:45:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,19 @@
-#include "alx.h"
-
-
+#include "main.h"
 
 /**
+ * main - prints _putchar and a new line
+ * Return: Always 0
+ */
+int main(void)
 
-* print_alphabet - Print the alphabet in lowercase.
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-*
-
-* Return: void.
-
-*/
-
-
-
-void print_alphabet(void)
-
-{
-
-	int i;
-
-	for (i = 97; i < 123; i++)
-
-		{
-
-		_putchar(i);
-
-		}
-
-		_putchar('\n');
-
-}
+	return (0);
