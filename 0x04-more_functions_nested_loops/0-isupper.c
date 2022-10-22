93b2343fe22ed734c3f0b554faad49ded39d9b62
Gratien1 <gratiensossouzin1@gmail.com>
2022-10-22 09:22:28 -0700

    fix main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: Variable text
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c 0 >= 'A' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
