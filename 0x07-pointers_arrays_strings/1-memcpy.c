5d75b9adcdd775093d2cf093119b2e0b808e2111
Gratien1 <gratiensossouzin1@gmail.com>
2022-11-01 02:38:38 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,3 @@
-#include "stdio.h"
 #include "main.h"
 
 /**
@@ -12,8 +11,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-		for (i = 0; i < n; i++)
-			dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
-			return (dest);
+	return (dest);
 }
