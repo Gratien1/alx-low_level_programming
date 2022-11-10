8d90d5728fe1f88610ba452dc2251fd22642ccf0
Gratien1 <gratiensossouzin1@gmail.com>
2022-11-09 16:24:58 -0800

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -32,19 +32,21 @@ char *argstostr(int ac, char **av)
 	{
 		free(aout);
 		return (NULL);
-
-		for (i = j = ia = 0; ia < c; j++, ia++)
-		{
-			if (av[i][j] == '\0')
-			{
-				aout[ia] = '\n';
-				i++;
-				ia++;
-				j = 0;
-			}
-			if (ia < c - 1)
-				aout[ia] = av[i][j];
-		}        aout[ia] = '\0';
-
-		return (aout);
 	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
+}
